bbc487f44d7908ba68ef9f5024174ab73e397435
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-04 15:13:55 +0900

    build.gradle の kts 化と buildSrc の導入

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${versions.kotlin}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,9 @@
+// - kotlinpoet(https://github.com/square/kotlinpoet/blob/master/buildSrc/src/main/java/Dependencies.kt)の場合は
+//   buildSrc を利用して version, deps を定義している。picasso の方式を buildSrc に移行しただけっぽい感じですね。
+object versions {
+    const val kotlin = "1.3.72"
+}
+
+// TODO: 要検討
+//object deps {
+//}
