5eb3e68986197a6eac0a766c12bc868c8c68625a
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-06 08:53:08 +0900

    configure Component Selection Rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+// TODO: Gradle Version Plugin の設定を外部に出すか、外部に出せないことの証明のどちらかをやりたい：ここから
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.29.0"
+}
+// TODO: Gradle Version Plugin の設定を外部に出すか、外部に出せないことの証明のどちらかをやりたい：ここまで
+
 buildscript {
     repositories {
         google()
@@ -19,3 +27,20 @@ allprojects {
 task("clean", Delete::class) {
     delete = setOf(rootProject.buildDir)
 }
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// TODO: Gradle Version Plugin の設定を外部に出すか、外部に出せないことの証明のどちらかをやりたい：ここから
+// configure Component Selection Rules.
+// @See https://docs.gradle.org/current/userguide/dynamic_versions.html#sec:component_selection_rules
+tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+// TODO: Gradle Version Plugin の設定を外部に出すか、外部に出せないことの証明のどちらかをやりたい：ここまで
